fac0c92589f5f821d800d4a24002771002c83d52
Nikola Zlatinov <nikola.zlatinov@news.co.uk>
2021-08-27 11:01:16 +0300

    fix: add versel image and fix test

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+
 import Home from '../pages/index';
 
 describe('index page', () => {
-  it('should render correctly', () => {
-    const component = renderer.create(<Home />);
-    expect(component).toMatchSnapshot
+  it('should render correctly', async () => {
+    render(<Home />);
+    const expected = await screen.findByTestId('home-container')
+    expect(expected).toMatchSnapshot()
   })
 })
